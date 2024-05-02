4dcdf8976b2115dc34f5f4124e83c425af1562e0
Lazaro <lachy200408@gmail.com>
2024-05-01 23:29:47 -0400

    API Vanilla finished

---
+++ idGenerator.js
@@ -0,0 +1,5 @@
+const crypto = require('node:crypto')
+
+const id = crypto.randomUUID();
+
+console.log(id);
---
+++ server-vanilla.js
@@ -0,0 +1,36 @@
+const http = require('node:http')
+const menuJson = require('./menu.json')
+
+const tipoJson = { 'Content-Type': 'application/json; charset=utf-8' }
+
+function serverFucntion (req, res) {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+
+	if(req.method === 'GET'){
+		switch (req.url) {
+			case '/menu': {
+				res.writeHead(200, tipoJson)
+				res.end(JSON.stringify(menuJson))
+				break
+			}
+			case '/menu/kinds': {
+				const menuKindsJson = {
+					menuKinds: Object.keys(menuJson)
+				}
+
+				res.writeHead(200,tipoJson)
+				res.end(JSON.stringify(menuKindsJson))
+				break
+			}
+			default: {
+				res.writeHead(404, tipoJson)
+				res.end(JSON.stringify({ message: "No se encontro el resurso solicitado." }))
+			}
+		}
+	}
+}
+
+const server = http.createServer(serverFucntion);
+server.listen(3000, () => {
+	console.log('Server is running on http://localhost:3000');
+})
